52bfd2a7c47757d03d3289e0f1025adb76ce8ff5
ivaosthu <ivar.conradi.osthus@finn.no>
2019-08-31 13:02:07 +0200

    remove browser field

--- examples/react-app/src/index.js
+++ examples/react-app/src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import UnleashClient from 'unleash-proxy-client';
+import * as unleash from 'unleash-proxy-client/build/index.js';
 
+console.log(unleash);
+/*
 var config = {url: 'https://eu.unleash-hosted.com/hosted/api', clientKey: 'lkasjda'};
 var context = {userId: '1233'};
 
 var client = new UnleashClient(config, context);
 client.start();
-
-ReactDOM.render(<App unleash={client} />, document.getElementById('root'));
+*/
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

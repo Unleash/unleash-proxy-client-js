f2601d2014b6c194996013b8b41901bad0020641
Ivar Conradi Østhus <ivarconr@gmail.com>
2021-06-08 21:16:04 +0200

    use AsyncStorage to make it usable for react native as well.  (#22)
    * Adding react-native async-storage
    
    Adding react-native async-storage to set and get items as window.localStorage is not applicable for mobile apps
    
    * Update package-lock.json
    
    * Adding storage support for react-native
    
    * fix: use @react-native-community/async-storage also for web
    
    Co-authored-by: vedyed <81049702+vedyed@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -8,5 +8,5 @@ module.exports = {
     "automock": false,
     "setupFiles": [
       "./setupJest.js"
-    ]
+    ],
   }
--- src/storage-provider-local.ts
+++ src/storage-provider-local.ts
@@ -1,23 +1,23 @@
+import AsyncStorage from "@react-native-community/async-storage";
 import IStorageProvider from './storage-provider';
 
 export default class LocalStorageProvider implements IStorageProvider {
     private prefix = 'unleash:repository';
 
-    public save(name: string, data: any) {
+    public async save(name: string, data: any) {
+        const repo = JSON.stringify(data);
+        const key = `${this.prefix}:${name}`;
         try {
-            const repo = JSON.stringify(data);
-            const key = `${this.prefix}:${name}`;
-            window.localStorage.setItem(key, repo);
-        } catch(e) {
-            // tslint:disable-next-line
-            console.error(e);
+            await AsyncStorage.setItem(key, repo);
+        } catch(ex) {
+            console.error(ex);  
         }
     }
 
-    public get(name: string) {
+    public async get(name: string) {
         try {
             const key = `${this.prefix}:${name}`;
-            const data = window.localStorage.getItem(key);
+            const data = await AsyncStorage.getItem(key);
             return data ? JSON.parse(data) : undefined;
         } catch(e) {
             // tslint:disable-next-line

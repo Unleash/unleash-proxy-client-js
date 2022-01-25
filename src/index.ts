f96a627b9b791f26c9d5eb2083293f2241dd6eca
Fredrik Oseberg <fredrik.no@gmail.com>
2022-01-25 11:00:00 +0100

    fix: add test for bootstrap ready event

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ interface IConfig extends IStaticContext {
     fetch?: any;
     bootstrap?: IToggle[];
     bootstrapOverride?: boolean;
-    headerName?: string
+    headerName?: string;
 }
 
 interface IVariant {
@@ -105,7 +105,7 @@ export class UnleashClient extends TinyEmitter {
         fetch = resolveFetch(),
         bootstrap,
         bootstrapOverride = true,
-        headerName = 'Authorization'
+        headerName = 'Authorization',
     }: IConfig) {
         super();
         // Validations
@@ -155,7 +155,7 @@ export class UnleashClient extends TinyEmitter {
             url,
             clientKey,
             fetch,
-            headerName
+            headerName,
         });
     }
 
@@ -228,8 +228,8 @@ export class UnleashClient extends TinyEmitter {
         ) {
             await this.storage.save(storeKey, this.bootstrap);
             this.toggles = this.bootstrap;
+            this.emit(EVENTS.READY);
         }
-
         this.emit(EVENTS.INIT);
     }
 

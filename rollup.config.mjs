7e8391a80666fee46c248ab99d0d7265a6e7b40b
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2023-03-07 11:28:39 +0100

    update rollup plugins

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,29 +1,30 @@
 import typescript from 'rollup-plugin-typescript2';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
 
 import nodePolyfills from 'rollup-plugin-node-polyfills';
 
-
 export default {
-  input: './src/index.ts',
-  output: [
-    {
-     file: './build/main.min.js',
-     format: 'umd',
-     name: 'unleash' // the global which can be used in a browser
-    }
-   ],
-  plugins: [
-    nodePolyfills(),
-    resolve({
-        browser: true
-    }),
-    commonjs({ // rollup-plugin-commonjs
-        include: 'node_modules/**'
-    }),
-    typescript({tsconfigOverride:{compilerOptions: {module: "ES2015" }}}),
-    terser()
-  ]
-}
+    input: './src/index.ts',
+    output: [
+        {
+            file: './build/main.min.js',
+            format: 'umd',
+            name: 'unleash', // the global which can be used in a browser
+        },
+    ],
+    plugins: [
+        nodePolyfills(),
+        nodeResolve({
+            browser: true,
+        }),
+        commonjs({
+            include: 'node_modules/**',
+        }),
+        typescript({
+            tsconfigOverride: { compilerOptions: { module: 'ES2015' } },
+        }),
+        terser(),
+    ],
+};

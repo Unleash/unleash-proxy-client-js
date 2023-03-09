1ab4a90d3587065a916ec4c06c800501a2fcbcd6
Thomas Heartman <thomas@getunleash.ai>
2023-03-09 11:34:52 +0100

    format: format test suite

--- src/util.test.ts
+++ src/util.test.ts
@@ -1,7 +1,7 @@
-import {urlWithContextAsQuery} from './util';
+import { urlWithContextAsQuery } from './util';
 
 test('should not add paramters to URL', async () => {
-    const someUrl = new URL("https://test.com");
+    const someUrl = new URL('https://test.com');
 
     //@ts-expect-error on purpose for testing!
     const result = urlWithContextAsQuery(someUrl, {});
@@ -10,19 +10,29 @@ test('should not add paramters to URL', async () => {
 });
 
 test('should add context as query params', async () => {
+    const someUrl = new URL('https://test.com');
 
-    const someUrl = new URL("https://test.com");
+    const result = urlWithContextAsQuery(someUrl, {
+        appName: 'test',
+        userId: '1234A',
+    });
 
-    const result = urlWithContextAsQuery(someUrl, {appName: 'test', userId: '1234A'});
-
-    expect(result.toString()).toBe('https://test.com/?appName=test&userId=1234A');
+    expect(result.toString()).toBe(
+        'https://test.com/?appName=test&userId=1234A'
+    );
 });
 
 test('should add context properties as query params', async () => {
+    const someUrl = new URL('https://test.com');
 
-    const someUrl = new URL("https://test.com");
+    const result = urlWithContextAsQuery(someUrl, {
+        appName: 'test',
+        userId: '1234A',
+        properties: { custom1: 'test', custom2: 'test2' },
+    });
 
-    const result = urlWithContextAsQuery(someUrl, {appName: 'test', userId: '1234A', properties: { custom1: 'test', custom2: "test2"}});
-
-    expect(result.toString()).toBe('https://test.com/?appName=test&userId=1234A&properties%5Bcustom1%5D=test&properties%5Bcustom2%5D=test2');
+    expect(result.toString()).toBe(
+        'https://test.com/?appName=test&userId=1234A&properties%5Bcustom1%5D=test&properties%5Bcustom2%5D=test2'
+    );
 });
+

520c25d2e77bea5001b0c15aef55c44e5f4a555e
Thomas Heartman <thomas@getunleash.ai>
2023-03-16 09:35:34 +0100

    fix: add initial tests for custom headers

--- src/metrics.test.ts
+++ src/metrics.test.ts
@@ -62,7 +62,7 @@ test('should send metrics', async () => {
     expect(body.bucket.toggles.bar.no).toEqual(1);
 });
 
-test('should send metrics under custom header', async () => {
+test('should send metrics with custom auth header', async () => {
     const metrics = new Metrics({
         onError: console.error,
         appName: 'test',
@@ -144,4 +144,72 @@ test('should send metrics based on timer interval', async () => {
     jest.advanceTimersByTime(5000);
 
     expect(fetchMock.mock.calls.length).toEqual(3);
+});
+
+test('Custom headers for metrics', () => {
+    const runMetrics = async (customHeaders) => {
+        const metrics = new Metrics({
+            onError: console.error,
+            appName: 'test',
+            metricsInterval: 5,
+            disableMetrics: false,
+            url: 'http://localhost:3000',
+            clientKey: '123',
+            fetch: fetchMock,
+            headerName: 'Authorization',
+            customHeaders,
+        });
+
+        metrics.count('foo', true);
+        await metrics.sendMetrics();
+
+        return getTypeSafeRequest(fetchMock);
+    };
+
+    test('Should apply any custom headers to the metrics request', async () => {
+        const customHeaders = {
+            'x-custom-header': '123',
+        };
+
+        const requestBody = await runMetrics(customHeaders);
+        expect(requestBody.headers?.['x-custom-header']).toEqual(
+            customHeaders['x-custom-header']
+        );
+    });
+
+    test('Custom headers should override preset headers', async () => {
+        const customHeaders = {
+            Authorization: 'definitely-not-the-client-key',
+        };
+
+        const requestBody = await runMetrics(customHeaders);
+
+        expect(requestBody.headers?.['Authorization']).toEqual(
+            customHeaders['Authorization']
+        );
+    });
+
+    test('Empty custom headers do not override preset headers on collision', async () => {
+        const customHeaders = {
+            Authorization: null,
+        };
+
+        const requestBody = await runMetrics(customHeaders);
+
+        expect(requestBody.headers?.['Authorization']).not.toBeUndefined();
+    });
+
+    test.each(['', null, undefined])(
+        'Custom headers that are %s should not be sent',
+        async (emptyValue) => {
+            const customHeaders = {
+                'invalid-header': emptyValue,
+            };
+
+            const requestBody = await runMetrics(customHeaders);
+
+            //ts-expect-error invalid-header isn't  a standard header.
+            expect(requestBody.headers?.['invalid-header']).toBeUndefined();
+        }
+    );
 });

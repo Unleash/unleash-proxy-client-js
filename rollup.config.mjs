93a1cb36e2fdf8d41853a535761048071ffae294
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2023-06-12 17:30:11 +0200

    build cjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,7 @@
-import typescript from 'rollup-plugin-typescript2';
+import typescript from '@rollup/plugin-typescript';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
-
 import nodePolyfills from 'rollup-plugin-node-polyfills';
 
 export default {
@@ -13,20 +12,17 @@ export default {
             format: 'umd',
             sourcemap: true,
             name: 'unleash', // the global which can be used in a browser
-            plugins: [terser()],
-        },
-        {
-            file: './build/cjs/index.js',
-            sourcemap: true,
-            format: 'cjs',
-        },
-        {
-            file: './build/esm/index.mjs',
-            sourcemap: true,
-            format: 'es',
         },
     ],
     plugins: [
+        typescript({
+            compilerOptions: {
+                lib: ['es5', 'es6', 'dom'],
+                module: 'ES2015',
+                target: 'es5',
+            },
+            sourceMap: true,
+        }),
         nodePolyfills(),
         nodeResolve({
             browser: true,
@@ -34,8 +30,6 @@ export default {
         commonjs({
             include: 'node_modules/**',
         }),
-        typescript({
-            tsconfigOverride: { compilerOptions: { module: 'ES2015' } },
-        }),
+        terser(),
     ],
 };
